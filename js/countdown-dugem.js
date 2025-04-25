22b90e9bb56520418ea5d309dbbd136ec9e27aec
aOfztiMus <amornchet_s@outlook.com>
2025-04-25 23:52:26 +0700

    Update index and single blog

--- js/countdown-dugem.js
+++ js/countdown-dugem.js
@@ -7,7 +7,7 @@
          hour = minute * 60,
          day = hour * 24;
 
-         let birthday = "Dec 31, 2021 00:00:00",
+         let birthday = "May 30, 2025 00:00:00",
          countDown = new Date(birthday).getTime(),
          x = setInterval(function() {
 
